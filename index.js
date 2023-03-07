93f3d4449f15f860fe389a11f49b7a861d7fb314
Pedro Martins <ppedro.dev@hotmail.com>
2023-03-07 17:59:40 -0300

    :tada initial comit

---
+++ index.js
@@ -0,0 +1,33 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    // if(body.classList.contains('light')){
+    //     html.classList.remove('light')
+    // } else {
+    //     html.classList.add('light')
+    // }
+
+    html.classList.toggle('light')
+
+    //TROCAR A IMAGEM 
+    // pegar a tag img e substituir a img
+    const img = document.querySelector('#profile img')
+    
+    // Substituir a imagem
+    if(html.classList.contains('light')) {
+        // se tiver light mode adicionar a imagem light
+        img.setAttribute('src', './assets/Avatar.png')
+    } else {
+        // se tiver sem o light mode manter a imagem normal
+        img.setAttribute('src', './assets/Avatar1.png')
+    }
+
+    const alt = document.querySelector('#profile img')
+    if(html.classList.contains('light')){
+        img.setAttribute('alt', 'Imagem versao light mode')
+    } else {
+        img.setAttribute('alt', 'Imagem versao dark mode')
+    }
+}
+
+
